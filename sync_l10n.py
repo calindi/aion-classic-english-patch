e3f68e374042faa0ce087b14c6c9e32e8aa50536
calindi <mangespam@gmail.com>
2020-12-15 19:02:36 +0100

    Add variant to send KR messages when applying to groups or refusing people

--- sync_l10n.py
+++ sync_l10n.py
@@ -1,6 +1,6 @@
 # Python 3.8+ with type annotations and dataclasses
 from __future__ import annotations
-import os, shutil, codecs, enum, re
+import os, shutil, codecs, enum, re, argparse
 from dataclasses import dataclass
 from typing import List, Dict, Optional
 
@@ -9,6 +9,10 @@ CLIENT_DIR         = os.path.join(SCRIPT_DIR, 'client')
 L10N_REFERENCE_DIR = os.path.join(SCRIPT_DIR, 'l10n_reference')
 L10N_PATCH_DIR     = os.path.join(SCRIPT_DIR, 'l10n_patch')
 OUTPUT_DIR         = os.path.join(SCRIPT_DIR, 'output')
+
+VARIANT_KRLFG      = os.path.join(SCRIPT_DIR, 'variant_krlfg')
+OUTPUT_KRLFG       = os.path.join(SCRIPT_DIR, 'output_krlfg')
+
 
 @dataclass
 class XMLElement:
@@ -146,7 +150,7 @@ class AionString:
     ment: Optional[str]
     rank: Optional[int]
 
-    def match_and_repair(self, other: AionString) -> bool:
+    def match_and_repair(self, other: AionString, silent: bool =False) -> bool:
         if self.id_value != other.id_value:
             print(f"[critical] <id> mismatch: client: {self.id_value}, L10N: {other.id_value}")
             return False
@@ -157,32 +161,39 @@ class AionString:
 
         # Repair mismatching values
         if self.message_type != other.message_type:
-            print(f"[action] {self.id_value}|{self.name}: repairing <message_type>: {self.message_type}, L10N: {other.message_type}")
+            if not silent:
+                print(f"[action] {self.id_value}|{self.name}: repairing <message_type>: {self.message_type}, L10N: {other.message_type}")
             other.message_type = self.message_type
 
         if self.display_type != other.display_type:
-            print(f"[action] {self.id_value}|{self.name}: repairing <display_type>: client: {self.display_type}, L10N: {other.display_type}")
+            if not silent:
+                print(f"[action] {self.id_value}|{self.name}: repairing <display_type>: client: {self.display_type}, L10N: {other.display_type}")
             other.display_type = self.display_type
 
         if self.ment != other.ment:
-            print(f"[action] {self.id_value}|{self.name}: repairing <ment>: client: {self.ment}, L10N: {other.ment}")
+            if not silent:
+                print(f"[action] {self.id_value}|{self.name}: repairing <ment>: client: {self.ment}, L10N: {other.ment}")
             other.ment = self.ment
 
         if self.rank != other.rank:
-            print(f"[action] {self.id_value}|{self.name}: repairing <rank>: client: {self.rank}, L10N: {other.rank}")
+            if not silent:
+                print(f"[action] {self.id_value}|{self.name}: repairing <rank>: client: {self.rank}, L10N: {other.rank}")
             other.rank = self.rank
 
         # match expressions
         if self.body is None and other.body is not None and other.body != '':
-            print(f"[warn] {self.id_value}|{self.name}: repairing <body>: client <body> does not exist, but L10N <body> exists: '{other.body}' !")
+            if not silent:
+                print(f"[warn] {self.id_value}|{self.name}: repairing <body>: client <body> does not exist, but L10N <body> exists: '{other.body}' !")
             other.body = None
         elif self.body is not None and self.body != '' and other.body is None:
-            print(f"[error] {self.id_value}|{self.name}: <body> mismatch: client <body> exists: '{self.body}', but L10N <body> does not exist!")
+            if not silent:
+                print(f"[error] {self.id_value}|{self.name}: <body> mismatch: client <body> exists: '{self.body}', but L10N <body> does not exist!")
         elif self.body is not None and other.body is not None:
             client_exprs = set(EXPRESSION_RE.findall(self.body))
             l10n_exprs = set(EXPRESSION_RE.findall(other.body))
             if client_exprs != l10n_exprs:
-                print(f"[warn] {self.id_value}|{self.name}: <body> expression mismatch: client: {client_exprs}, L10N: {l10n_exprs}")
+                if not silent:
+                    print(f"[warn] {self.id_value}|{self.name}: <body> expression mismatch: client: {client_exprs}, L10N: {l10n_exprs}")
 
         return True
 
@@ -280,85 +291,150 @@ def case_insensitive_path(base_path: str, rel_path: str):
             base_path = os.path.join(base_path, path_component)
     return base_path
 
-def sync_strings(relpath: str, tag="strings"):
+def sync_strings(relpath: str, client_dir: str, reference_dir: str, patch_dir: str, output_dir: str, variant_dir: Optional[str], tag: str, silent: bool):
     print()
     print()
     print(f"Checking file '{relpath}'")
 
-    client_dict = AionStringDict.read(case_insensitive_path(CLIENT_DIR, relpath))
-    l10n_reference__dict = AionStringDict.read(case_insensitive_path(L10N_REFERENCE_DIR, relpath))
-    l10n_patch_dict = AionStringDict.read(case_insensitive_path(L10N_PATCH_DIR, relpath), allow_missing=True)
+    client_dict = AionStringDict.read(case_insensitive_path(client_dir, relpath))
+    l10n_reference__dict = AionStringDict.read(case_insensitive_path(reference_dir, relpath))
+    l10n_patch_dict = AionStringDict.read(case_insensitive_path(patch_dir, relpath), allow_missing=True)
 
     # Merge english and custom patch dict
-    l10n_dict = AionStringDict({**l10n_reference__dict.strings, **l10n_patch_dict.strings})
+    if variant_dir is not None:
+        l10n_variant_dict = AionStringDict.read(case_insensitive_path(variant_dir, relpath), allow_missing=True)
+        l10n_dict = AionStringDict({**l10n_reference__dict.strings, **l10n_patch_dict.strings, **l10n_variant_dict.strings})
+    else:
+        l10n_dict = AionStringDict({**l10n_reference__dict.strings, **l10n_patch_dict.strings})
 
     client_dict_keys = set(client_dict.strings.keys())
     l10n_dict_keys = set(l10n_dict.strings.keys())
 
     # l10n strings not in client
     for k in l10n_dict_keys.difference(client_dict_keys):
-        print(f"[warn] {k}|{l10n_dict.strings[k].name} exists in l10n but not in client")
+        if not silent:
+            print(f"[warn] {k}|{l10n_dict.strings[k].name} exists in l10n but not in client")
         # Clear key from dictionaries before outputting files
         l10n_dict.strings.pop(k, None)
         l10n_patch_dict.strings.pop(k, None)
 
     # client strings not handled by l10n
     for k in client_dict_keys.difference(l10n_dict_keys):
-        print(f"{k}|{client_dict.strings[k].name} MISSING from l10n!")
+        if not silent:
+            print(f"{k}|{client_dict.strings[k].name} MISSING from l10n!")
         l10n_patch_dict.strings[k] = client_dict.strings[k]
 
     # check mismatch between kor/eng
     for k in client_dict_keys.intersection(l10n_dict_keys):
-        if not client_dict.strings[k].match_and_repair(l10n_dict.strings[k]):
+        if not client_dict.strings[k].match_and_repair(l10n_dict.strings[k], silent=silent):
             l10n_patch_dict.strings[k] = client_dict.strings[k]
 
-    # update patch dictionary file
-    if len(l10n_patch_dict.strings) > 0:
-        l10n_patch_dict.write(os.path.join(L10N_PATCH_DIR, relpath), tag)
+    if variant_dir is None:
+        # update patch dictionary file
+        if len(l10n_patch_dict.strings) > 0:
+            l10n_patch_dict.write(os.path.join(patch_dir, relpath), tag)
 
     # output translation file
     output_dict = AionStringDict({**l10n_dict.strings, **l10n_patch_dict.strings})
-    output_dict.write(os.path.join(OUTPUT_DIR, relpath), tag)
+    output_dict.write(os.path.join(output_dir, relpath), tag)
     
-def main():
-    if os.path.exists(OUTPUT_DIR):
-        shutil.rmtree(OUTPUT_DIR)
-    os.makedirs(OUTPUT_DIR)
+def sync_all_strings(client_dir: str, reference_dir: str, patch_dir: str, output_dir: str, variant_dir: str, silent: bool):
+    STRING_FILES = [
+        'client_strings_bm.xml',
+        'client_strings_bmrestrict.xml',
+        'client_strings_dic_etc.xml',
+        'client_strings_dic_item.xml',
+        'client_strings_dic_monster.xml',
+        'client_strings_dic_people.xml',
+        'client_strings_dic_place.xml',
+        'client_strings_etc.xml',
+        'client_strings_funcpet.xml',
+        'client_strings_gossip.xml',
+        'client_strings_item.xml',
+        'client_strings_item2.xml',
+        'client_strings_item2.xml',
+        'client_strings_monster.xml',
+        'client_strings_msg.xml',
+        'client_strings_quest.xml',
+        'client_strings_skill.xml',
+        'client_strings_ui.xml',
+        'StringTable_Dialog.xml',
+    ]
 
-    sync_strings(os.path.join('data', 'strings', 'client_strings_bm.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_bmrestrict.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_dic_etc.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_dic_item.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_dic_monster.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_dic_people.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_dic_place.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_etc.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_funcpet.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_gossip.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_item.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_item2.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_level.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_monster.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_msg.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_npc.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_quest.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_skill.xml'))
-    sync_strings(os.path.join('data', 'strings', 'client_strings_ui.xml'))
-    sync_strings(os.path.join('data', 'strings', 'StringTable_Dialog.xml'))
-    sync_strings(os.path.join('data', 'strings', 'stringtable_tip.xml'), tag="string_tips")
+    for string_file in STRING_FILES:
+        sync_strings(
+            relpath=os.path.join('data', 'strings', string_file),
+            client_dir=client_dir,
+            reference_dir=reference_dir,
+            patch_dir=patch_dir,
+            output_dir=output_dir,
+            variant_dir=variant_dir,
+            tag="string",
+            silent=silent)
+
+    STRING_TIPS_FILES = [
+        'stringtable_tip.xml'
+    ]
+
+    for string_tip_file in STRING_TIPS_FILES:
+        sync_strings(
+            relpath=os.path.join('data', 'strings', string_file),
+            client_dir=client_dir,
+            reference_dir=reference_dir,
+            patch_dir=patch_dir,
+            output_dir=output_dir,
+            variant_dir=variant_dir,
+            tag="string_tips",
+            silent=silent)
+
+def make_package(client_dir: str, reference_dir: str, patch_dir: str, output_dir: str, variant_dir: str, silent: bool):
+
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
+
+    if variant_dir not None:
+        print("### Syncing standard package")
+    else:
+        print(f"### Syncing with variant: '{variant_dir}'")
+
+    sync_all_strings(
+        client_dir=client_dir,
+        reference_dir=reference_dir,
+        patch_dir=patch_dir,
+        output_dir=output_dir,
+        variant_dir=variant_dir,
+        silent=silent)
 
     print(f"Copying reference l10n textures to output directory...")
-    shutil.copytree(os.path.join(L10N_REFERENCE_DIR, 'textures'), os.path.join(OUTPUT_DIR, 'textures'))
+    shutil.copytree(os.path.join(reference_dir, 'textures'), os.path.join(output_dir, 'textures'))
     print(f"Copying reference l10n data\\dialogs to output directory...")
-    shutil.copytree(os.path.join(L10N_REFERENCE_DIR, 'data', 'dialogs'), os.path.join(OUTPUT_DIR, 'data', 'dialogs'))
+    shutil.copytree(os.path.join(reference_dir, 'data', 'dialogs'), os.path.join(output_dir, 'data', 'dialogs'))
     print(f"Copying reference l10n data\\cutscene to output directory...")
-    shutil.copytree(os.path.join(L10N_REFERENCE_DIR, 'data', 'cutscene'), os.path.join(OUTPUT_DIR, 'data', 'cutscene'))
+    shutil.copytree(os.path.join(reference_dir, 'data', 'cutscene'), os.path.join(output_dir, 'data', 'cutscene'))
     print(f"Copying reference l10n data\\strings\\error to output directory...")
-    shutil.copytree(os.path.join(L10N_REFERENCE_DIR, 'data', 'strings', 'error'), os.path.join(OUTPUT_DIR, 'data', 'strings', 'error'))
+    shutil.copytree(os.path.join(reference_dir, 'data', 'strings', 'error'), os.path.join(output_dir, 'data', 'strings', 'error'))
 
     print(f"Copying patch l10n data\\ui to output directory...")
-    shutil.copytree(os.path.join(L10N_PATCH_DIR, 'data', 'ui'), os.path.join(OUTPUT_DIR, 'data', 'ui'))
+    shutil.copytree(os.path.join(patch_dir, 'data', 'ui'), os.path.join(output_dir, 'data', 'ui'))
+
+def main():
+    make_package(
+        client_dir=CLIENT_DIR,
+        reference_dir=L10N_REFERENCE_DIR,
+        patch_dir=L10N_PATCH_DIR,
+        output_dir=OUTPUT_DIR,
+        variant_dir=None,
+        silent=False)
+
+    make_package(
+        client_dir=CLIENT_DIR,
+        reference_dir=L10N_REFERENCE_DIR,
+        patch_dir=L10N_PATCH_DIR,
+        output_dir=OUTPUT_KRLFG,
+        variant_dir=VARIANT_KRLFG,
+        silent=True)
 
 if __name__ == '__main__':
     main()
-    pass
